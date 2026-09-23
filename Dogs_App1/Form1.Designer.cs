812a4581f33326063a0ca8d414b2406565d33be9
nyakovlenko <nyakovlenko@gmail.com>
2026-09-23 13:32:43 +0500

    добавлено Редактировать: При отсутствии собак кнопки не должны нажиматься и наводиться

--- Dogs_App1/Form1.Designer.cs
+++ Dogs_App1/Form1.Designer.cs
@@ -264,7 +264,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
-            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dogBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
--- Dogs_App1/Form1.cs
+++ Dogs_App1/Form1.cs
@@ -12,12 +12,13 @@ namespace Dogs_App1
         public Form1()
         {
             InitializeComponent();
+            // dataSet1 уже создан в Form1.Designer.cs, здесь его объявлять не нужно!
         }
 
         private void SaveToXML()
         {
-            // Сохраняем и данные, и схему (важно для AutoIncrement)
             dataSet1.WriteXml(filePath, XmlWriteMode.WriteSchema);
+            UpdateButtonStates();
         }
 
         private void LoadFromXML()
@@ -34,63 +35,65 @@ namespace Dogs_App1
             LoadFromXML();
             dataGridView1.DataSource = dataSet1.Dog;
             dataGridView2.DataSource = dataSet1.TrainingSession;
+
+            // ВАЖНО: Блок с ручным изменением HeaderText УДАЛЕН. 
+            // Русские заголовки ("ЧипID", "Кличка" и т.д.) уже настроены в Form1.Designer.cs!
+
+            UpdateButtonStates();
         }
 
-        // Сохраняем данные при закрытии программы
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             SaveToXML();
         }
 
-        // 1. ИСПРАВЛЕННАЯ фильтрация тренировок при выборе собаки
+        private void UpdateButtonStates()
+        {
+            bool hasDogs = dataSet1.Dog.Rows.Count > 0;
+            bool hasTrainings = dataSet1.TrainingSession.Rows.Count > 0;
+
+            button2.Enabled = hasDogs;
+            button3.Enabled = hasDogs;
+            button4.Enabled = hasDogs;
+            button5.Enabled = hasDogs && hasTrainings;
+            button6.Enabled = hasDogs && hasTrainings;
+        }
+
         private void dataGridView1_SelectionChanged(object sender, EventArgs e)
         {
-            // Используем точное имя столбца Chip_ID из твоего Designer.cs
             if (dataGridView1.CurrentRow != null && dataGridView1.CurrentRow.Cells["Chip_ID"].Value != DBNull.Value)
             {
                 string selectedChipId = dataGridView1.CurrentRow.Cells["Chip_ID"].Value.ToString();
-
-                // Используем точное имя столбца FK_Chip_ID
                 (dataGridView2.DataSource as DataView).RowFilter = $"FK_Chip_ID = '{selectedChipId}'";
             }
             else
             {
-                // Если собака не выбрана, показываем все тренировки
                 (dataGridView2.DataSource as DataView).RowFilter = "";
             }
         }
 
-        // 2. ЭТОТ МЕТОД УБИРАЕТ ОШИБКУ КОМПИЛЯЦИИ
-        // Он нужен, потому что указан в Designer.cs, но раньше его не было в коде
         private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            // Пока оставляем пустым
+            // Оставляем пустым, как требует Designer
         }
 
-        // === КНОПКИ ДЛЯ СОБАК ===
-
-        // Кнопка 1: Создать карточку собаки
         private void button1_Click(object sender, EventArgs e)
         {
-            FormDog form = new FormDog(dataSet1, true); // true = режим добавления
+            FormDog form = new FormDog(dataSet1, true);
             if (form.ShowDialog() == DialogResult.OK)
             {
                 SaveToXML();
             }
         }
 
-        // Кнопка 2: Редактировать карточку собаки
         private void button2_Click(object sender, EventArgs e)
         {
             if (dataGridView1.CurrentRow != null)
             {
-                // Получаем строку данных через DataBoundItem (надёжнее!)
                 DataRowView rowView = dataGridView1.CurrentRow.DataBoundItem as DataRowView;
                 if (rowView != null)
                 {
                     string chipId = rowView.Row["Chip_ID"].ToString();
-
-                    // Открываем FormDog в режиме редактирования
                     FormDog form = new FormDog(dataSet1, false, chipId);
                     if (form.ShowDialog() == DialogResult.OK)
                     {
@@ -104,7 +107,6 @@ namespace Dogs_App1
             }
         }
 
-        // Кнопка 3: Удалить карточку собаки
         private void button3_Click(object sender, EventArgs e)
         {
             if (dataGridView1.CurrentRow != null)
@@ -112,23 +114,17 @@ namespace Dogs_App1
                 if (MessageBox.Show("Вы уверены? Это также удалит все связанные тренировки этой собаки.",
                     "Подтверждение", MessageBoxButtons.YesNo) == DialogResult.Yes)
                 {
-                    // Получаем Chip_ID через DataBoundItem
                     DataRowView rowView = dataGridView1.CurrentRow.DataBoundItem as DataRowView;
                     if (rowView != null)
                     {
                         string chipId = rowView.Row["Chip_ID"].ToString();
-
-                        // Сначала удаляем все тренировки этой собаки
                         DataRow[] relatedTrainings = dataSet1.TrainingSession.Select($"FK_Chip_ID = '{chipId}'");
                         foreach (DataRow training in relatedTrainings)
                         {
                             training.Delete();
                         }
-
-                        // Затем удаляем саму собаку
                         rowView.Row.Delete();
                     }
-
                     SaveToXML();
                 }
             }
@@ -138,41 +134,32 @@ namespace Dogs_App1
             }
         }
 
-        // === КНОПКИ ДЛЯ ТРЕНИРОВОК ===
-
-        // Кнопка 4: Создать запись о тренировке
         private void button4_Click(object sender, EventArgs e)
         {
-            // Проверяем, есть ли собаки, иначе нечего тренировать
             if (dataSet1.Dog.Rows.Count == 0)
             {
                 MessageBox.Show("Сначала добавьте хотя бы одну собаку!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 return;
             }
 
-            // Открываем форму добавления тренировки (false = режим добавления)
             FormTraining form = new FormTraining(dataSet1, false);
             if (form.ShowDialog() == DialogResult.OK)
             {
-                SaveToXML(); // Сохраняем, если пользователь нажал "Сохранить"
+                SaveToXML();
             }
         }
 
-
-        // Кнопка 5: Редактировать запись о тренировке
         private void button5_Click(object sender, EventArgs e)
         {
             if (dataGridView2.CurrentRow != null)
             {
                 try
                 {
-                    // Получаем строку данных надёжным способом
                     DataRowView rowView = dataGridView2.CurrentRow.DataBoundItem as DataRowView;
                     if (rowView != null)
                     {
                         int trainingId = -1;
 
-                        // Ищем столбец с ID тренировки (пробуем разные варианты названий)
                         if (rowView.Row.Table.Columns.Contains("Training_ID"))
                             trainingId = Convert.ToInt32(rowView.Row["Training_ID"]);
                         else if (rowView.Row.Table.Columns.Contains("TrainingID"))
@@ -180,9 +167,8 @@ namespace Dogs_App1
                         else if (rowView.Row.Table.Columns.Contains("ID"))
                             trainingId = Convert.ToInt32(rowView.Row["ID"]);
                         else
-                            trainingId = Convert.ToInt32(rowView.Row[0]); // Берём самый первый столбец
+                            trainingId = Convert.ToInt32(rowView.Row[0]);
 
-                        // Открываем форму редактирования
                         FormTraining form = new FormTraining(dataSet1, true, trainingId);
                         if (form.ShowDialog() == DialogResult.OK)
                         {
@@ -201,7 +187,6 @@ namespace Dogs_App1
             }
         }
 
-        // Кнопка 6: Удалить запись о тренировке
         private void button6_Click(object sender, EventArgs e)
         {
             if (dataGridView2.CurrentRow != null)
@@ -219,13 +204,8 @@ namespace Dogs_App1
             }
             else
             {
-                MessageBox.Show("Выберите тренировку для удаления.");
+                MessageBox.Show("Выберите тренировку для удаления.", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-        }
-
-        private void Form1_Load_1(object sender, EventArgs e)
-        {
-
         }
     }
 }
